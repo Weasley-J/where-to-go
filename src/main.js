dbf2458f6ce32c15508b62023880f9d4ad237e9b
julian <julxxy@outlook.com>
2024-09-15 12:42:12 +0800

    - Reformat code

--- src/main.js
+++ src/main.js
@@ -13,11 +13,10 @@ import { Swiper, SwiperSlide } from 'swiper/vue'
 FastClick.attach(document.body)
 
 function isDebugEnabled() {
-  let mode = import.meta.env.MODE
   return !!import.meta.env.VITE_IS_DEBUG_ENABLE
 }
 
-export const { isDebugEnable } = isDebugEnabled()
+export const isDebugEnable = isDebugEnabled()
 
 const app = createApp(App)
 app.config.globalProperties.isDebugEnable = isDebugEnabled()
