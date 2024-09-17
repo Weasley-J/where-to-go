2259dc0154ff68032a9feaf6f2966bf375d53c73
weasley <1432689025@qq.com>
2024-09-17 14:43:16 +0800

    - List scroll

--- src/components/city/List.vue
+++ src/components/city/List.vue
@@ -1,8 +1,7 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import { useStore } from 'vuex'
-
-const store = useStore()
+import BetterScroll from 'better-scroll'
 
 const props = defineProps({
   cityModules: {
@@ -14,6 +13,10 @@ const props = defineProps({
     default: () => []
   }
 })
+
+const store = useStore()
+const wrapper = ref(null)
+let scroll = null
 const provinceList = computed(() => {
   const domestic = props.cityModules.domestic || {} // 确保 domestic 为对象
   return Object.entries(domestic)
@@ -30,40 +33,68 @@ const provinceList = computed(() => {
     )
     .sort((a, b) => a.name.localeCompare(b.name))
 })
+
+watch(
+  () => props.currentCities,
+  (value, oldValue, onCleanup) => {
+    if (value && scroll) {
+      scroll.refresh()
+    }
+  }
+)
+
+onMounted(() => {
+  scroll = new BetterScroll(wrapper.value, {
+    scrollY: true,
+    click: true
+  })
+  // 如果内容可能是动态加载的，可以监听数据变化
+  // 在数据更新后重新刷新 BetterScroll
+  setTimeout(() => {
+    scroll.refresh() // 确保内容更新后 BetterScroll 刷新
+  }, 500)
+})
+onBeforeUnmount(() => {
+  if (scroll) {
+    scroll.destroy()
+  }
+})
 </script>
 
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div
-          v-for="({ flag, name, pinyin, cityCode }, index) of provinceList"
-          :key="index"
-          class="button-wrapper"
-        >
-          <div class="button">{{ name }}</div>
+  <div ref="wrapper" class="list">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div
+            v-for="({ flag, name, pinyin, cityCode }, index) of provinceList"
+            :key="index"
+            class="button-wrapper"
+          >
+            <div class="button">{{ name }}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div
-          v-for="({ sightId, placeName, imgUrl }, index) in currentCities"
-          :key="sightId"
-          class="button-wrapper"
-        >
-          <div class="button">{{ placeName }}</div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div
+            v-for="({ sightId, placeName, imgUrl }, index) in currentCities"
+            :key="sightId"
+            class="button-wrapper"
+          >
+            <div class="button">{{ placeName }}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div v-for="(values, key) in cityModules.domestic" :key="key" class="area">
-      <div class="title border-topbottom">{{ key }}</div>
-      <div class="item-list">
-        <ul v-for="({ name, pinyin }, index) in values" :key="index">
-          <li class="item border-top">{{ name }}</li>
-        </ul>
+      <div v-for="(values, key) in cityModules.domestic" :key="key" class="area">
+        <div class="title border-topbottom">{{ key }}</div>
+        <div class="item-list">
+          <ul v-for="({ name, pinyin }, index) in values" :key="index">
+            <li class="item border-top">{{ name }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -78,6 +109,7 @@ const provinceList = computed(() => {
   &::before
     background-color #cccccc
 
+
 .list
   overflow hidden
   position absolute
@@ -86,9 +118,9 @@ const provinceList = computed(() => {
   left 0
   bottom 0
 
-.area
   .title
     font-size .68rem
+    line-height .78rem
     color #666;
     background #eee;
     padding .3rem .4rem
