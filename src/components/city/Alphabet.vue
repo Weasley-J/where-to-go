79e1604393e22a529a1027b402c30e093b77edd1
weasley <1432689025@qq.com>
2024-09-18 01:38:22 +0800

    - Update city list

--- src/components/city/Alphabet.vue
+++ src/components/city/Alphabet.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onBeforeMount, ref, watch } from 'vue'
+import { onBeforeMount, onUpdated, ref, watch } from 'vue'
 import { isDebugEnable } from '@/debugEnable.js'
 import eventBus from '@/stores/eventBus.js'
 import { usePiniaStore } from '@/stores/usePiniaStore.js'
@@ -29,11 +29,20 @@ const currentCityLetter = ref('')
 const piniaStore = usePiniaStore()
 const touchStatus = ref(false)
 const letterElRefs = ref([]) // 定义存储 DOM 元素引用的对象
+const startY = ref(0) // 定义开始的 Y 坐标
+const touchTimer = ref(null)
+
 onBeforeMount(() => {
   // 排序: A - Z
   cityLetters.value = Object.keys(props.cityModules.domestic || {}).sort((a, b) =>
     a.localeCompare(b)
   )
+})
+
+onUpdated(() => {
+  if (letterElRefs.value) {
+    startY.value = letterElRefs.value['A'].offsetTop // 初始化起始 Y 坐标
+  }
 })
 
 // 监听 cityModules 变化
@@ -51,28 +60,54 @@ function handleClickCityLetter(alphabet) {
   piniaStore.updateCityLetter(alphabet) // Type 2
 }
 
-function handleTouchStar(e) {
+function handleTouchStar(event) {
   touchStatus.value = true
 }
 
 /**
  * 处理滚动事件
  */
-function handleTouchMove(e) {
-  if (touchStatus.value) {
-    let startY = letterElRefs.value['A'].offsetTop
-    let touchY = e.touches[0].clientY - 81 // 80 是字母 A 的高度
-    let distance = touchY - startY
-    let index = Math.floor(distance / 19)
+function handleTouchMove(event) {
+  // 如果 touchStatus 为 false，则不执行
+  if (!touchStatus.value) {
+    return
+  }
+
+  // 阻止默认事件，避免页面滚动
+  event.preventDefault()
+
+  // 清除之前的定时器，避免多次触发
+  if (touchTimer.value) {
+    clearTimeout(touchTimer.value)
+  }
+
+  // 字母 A 的偏移量和每个字母的高度
+  const alphabetOffset = 81 // 字母 A 的高度
+  const letterHeight = 19 // 每个字母的高度
+
+  // 设置定时器，防止快速滑动时触发多次点击事件
+  touchTimer.value = setTimeout(() => {
+    const { clientY } = event.touches[0] // 获取触摸点的 Y 坐标
+    const startY_ = startY.value
+    const touchY = clientY - alphabetOffset // 计算触摸位置相对于 A 的偏移量
+
+    // 根据触摸的位置计算字母索引
+    const distance = touchY - startY_
+    const index = Math.floor(distance / letterHeight)
+
     if (index >= 0 && index < cityLetters.value.length) {
-      let indexLetter = cityLetters.value[index]
-      if (isDebugEnable) console.log(`Handle scroll events：${startY} - ${index} - ${indexLetter}`)
+      const indexLetter = cityLetters.value[index]
+      if (isDebugEnable) {
+        console.log(
+          `Handle scroll events: startY=${startY_}, index=${index}, letter=${indexLetter}`
+        )
+      }
       handleClickCityLetter(indexLetter) // 触发点击事件
     }
-  }
+  }, 16) // 16ms 是一个典型的帧率间隔时间（60fps）
 }
 
-function handleTouchEnd(e) {
+function handleTouchEnd(event) {
   touchStatus.value = false
 }
 </script>
