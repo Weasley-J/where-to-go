b134a1b6a65da72aeef27d08ddf1d96afa9ec798
julian <julxxy@outlook.com>
2024-09-21 17:35:58 +0800

    - Add video-thumbnail

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { computed } from 'vue'
+
+// Don't cache
+const excludes = computed(() => {
+  return ['Detail']
+})
 </script>
 
 <template>
   <router-view v-slot="{ Component }">
-    <keep-alive>
+    <keep-alive :exclude="excludes">
       <component :is="Component" />
     </keep-alive>
   </router-view>
